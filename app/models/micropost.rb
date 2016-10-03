000041fdfce55269933cad17ebc91fe338f46dfb
ptnk1995 <nguyenphuoctiendn@gmail.com>
2016-10-03 13:52:23 +0700

    change validate

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -5,6 +5,6 @@ class Micropost < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
-  validates :title, presence: true, length: { maximum: 20 }
+  validates :title, presence: true, length: { maximum: 40 }
 
 end
